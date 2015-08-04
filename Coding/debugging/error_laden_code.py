95edaf9b8ed7f85cc89fd5731e99a4dfc5bc6f3f
eduvik <victor.rajewski@jmss.vic.edu.au>
2015-08-04 12:05:11 +1000

    initial commit

---
+++ Coding/debugging/error_laden_code.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+def average_of_list(1):
+  count = 0
+  sum = 0
+  for i in l:
+    sum += l
+  count += 1
+  return sum/count
+
+user_input = True
+the_list = []
+while user_input
+  the_list.append(user_input)
+  user_input = input("Please enter a number, or press enter to finish")
+
+print(Calculating...)
+avg = average_of_list(the_list)
+sleep(3)  # delay to make it seem realistic
+print ("The average is", avg)
